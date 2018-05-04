e05876dd6e87e6fa15ffa55b9dbc62031971988a
Zirui Li <ziruil@andrew.cmu.edu>
2018-05-03 22:24:01 -0400

    slightly modify database/seed.

--- lolcrawler.py
+++ lolcrawler.py
@@ -5,7 +5,7 @@ import time
 import random
 
 import cassiopeia as cass
-from cassiopeia import Summoner, Match
+from cassiopeia import Summoner, Match, Champions
 from cassiopeia.data import Season, Queue
 
 DIAMOND=10001
@@ -15,9 +15,50 @@ SLIVER=10001
 BRONZE=10001
 
 def initializeSeed(filename):
-    seeds=pd.read_csv(filename)
-    
-    pass
+    seeds=[]
+    raw_data=pd.read_csv(filename)
+    for idx,data in raw_data.iterrows():
+        for sommoner in data:
+            seeds.append(sommoner)
+    return seeds
+
+def getChampionsItemsAndSpells(conn):
+    c = conn.cursor()
+    # Get champions, in case the id of champions change because of version change,
+    # we sort it first
+    champions = Champions(region="NA")
+    champion_list=[]
+    for cham in champions:
+        champion_list.append(cham.name)
+    champion_list.sort()
+    champion2idx={}
+    for i,cham in enumerate(champion_list):
+        champion2idx[cham]=i
+        c.execute("INSERT INTO Champion VALUES( ?,? )",(i,cham))
+    # Get items
+    items = cass.get_items(region="NA")
+    item_list=[]
+    for item in items:
+        item_list.append(item.name)
+    item_list.sort()
+    item2idx={}
+    for i,item in enumerate(item_list):
+        item2idx[item]=i
+        c.execute("INSERT INTO item VALUES(?,?)",(i,item))
+    # Get Sommoner Spells
+    sspells = cass.get_summoner_spells(region="NA")
+    spell_list=[]
+    for spell in sspells:
+        spell_list.append(spell.name)
+    spell_list.sort()
+    spell2idx={}
+    for i,spell in enumerate(spell_list):
+        spell2idx[spell]=i
+        c.execute("INSERT INTO summoner_spell VALUES( ?,? )",(i, spell))
+    conn.commit()
+    return champion2idx,item2idx,spell2idx
+
+
 
 
 
@@ -30,15 +71,31 @@ def main():
     1. Initialize seedfiles
     2. Begin crawling
     """
-    diamond_count=10
-    platinum_count=10
-    gold_count=10
-    sliver_count=10
-    bronze_count=10
-    print('')
+    diamond_count=0
+    platinum_count=0
+    gold_count=0
+    sliver_count=0
+    bronze_count=0
+    nonrank_count=0
+    total_sommoner=0
+    match_total_num=0
+    match_repeat_num=0
+    match_invalid_num=0
+
+
+    conn = sqlite3.connect('lol.db')
     unpulled_summoners=initializeSeed('seed.csv')
+    # get champion, item, spells maps
+    champion2idx,item2idx,spell2idx=getChampionsItemsAndSpells(conn)
     while len(unpulled_summoners)>0:
+        is_enough=False
+        random.shuffle(unpulled_summoners)
+        for summoner in unpulled_summoners:
+            current_summoner=Summoner(name=summoner)
+
+        # whether we have got enough data
         if diamond_count<DIAMOND or platinum_count<PLATINUM or gold_count<GOLD or sliver_count<SLIVER or bronze_count<BRONZE:
+            is_enough=True
             break
 
     pass
@@ -48,5 +105,4 @@ def main():
 
 
 if __name__=='__main__':
-
     main()
