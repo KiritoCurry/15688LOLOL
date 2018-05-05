7ce535670f5d448431fd862294c24e7e19c643ad
Zirui Li <ziruil@andrew.cmu.edu>
2018-05-05 04:09:46 -0400

    Add to do list.

--- preprocessing.py
+++ preprocessing.py
@@ -12,6 +12,45 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+"""
+Todo:
+1. Basic data: Champions that are good at 
+(1). killing tower
+(2). first blood
+(3). monster KILLER(baron or dragon) [大龙杀手，小龙杀手，蓝爸爸红爸爸杀手], or 杀敌方野怪[最擅长入侵]最多
+(4). panta kill／。。。
+(5). 最肉tank [take most physical/magic//true demage ]
+(6). 最高输出 (physical/magic/true)
+(7). 补兵最多
+(8). 死的最多／存活率[时间]最高
+(9). 助攻王/击杀王
+(10). 控场王[timeCC]
+(11). 最容易连杀
+(12). 死亡频率较高[hidden senior case: 分析死亡数与消极游戏的联系]
+(13). most wards brought/killed (sight/vision) [插言／排眼，是一个好辅助的重要因素之一]
+(14). 最能奶的英雄(heal)
+(15). 红蓝方谁更容易赢
+(16). 推塔数／大龙数[baron]／小龙数[dragon]/峡谷先锋 与比赛输赢的关系
+(17). 暴击王
+2. 进阶data:
+(1). Big mistake, miss Skin!! Forgot to make it! [most popular skin, every hero's most popular skin, relationship between skin and win rate]
+(2). 英雄常用装备分析，推荐[游戏内已提供]
+(3). 英雄相克[交手战绩，杀／被杀次数，对线情况] ***
+(4). 最适合搭配队友[其实游戏中已被提供](可以个性化)  ****
+(5). 英雄走哪条路线胜率高 ***
+(6). 哪些是大后期，哪些后期废物(经济／输出比)
+(7). 利用kda，双方经济比，人头比预测比赛输赢  ****
+(8). 分析哪一路在游戏中最重要 (哪一路的双方经济／人头比对比赛结果影响最大)
+(9). 哪些英雄逆风局最容易翻盘(对敌方英雄输出最高，己方具有主要经济输出击杀等，己方总体经济／击杀落后于对方但最终取得胜利)
+(10). 英雄分类（战士，坦克，法师，射手，辅助，刺客。。。） *****
+
+3. 高级分析
+(1). 英雄个性化推荐
+(2). 召唤师技能[summoner spell]智能推荐,综合考虑每个英雄，走不同路线，对战不同英雄
+(3). 预测比赛输赢(同2.7)
+
+"""
+
 conn = sqlite3.connect('lol.db')
 def get_data(conn, table_name):
     res = []
@@ -110,10 +149,12 @@ def champion_data(champion_id, participants):
 participants = get_data(conn, 'Participants')
 champion = get_data(conn, 'Champion')
 ban = get_data(conn, 'team_ban')
+print(champion)
 
 all_data = []
 for c in champion:
     all_data.append(champion_data(c[0], participants))
+    print(c[1],champion_data(c[0], participants))
 print(all_data)
 
 
