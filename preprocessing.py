9adab0a5379febd029924a179b1085339c35f88a
Yaqi Peng <yachnepeng@gmail.com>
2018-05-05 01:25:03 -0400

    optimized initial code

--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,18 @@
 
 # coding: utf-8
 
-import sqlite3
-import pandas as pd
-import numpy as np
 # import cassiopeia as cass
+
 # from cassiopeia.data import Queue, GameMode, Season
 # from cassiopeia import SummonerSpell, SummonerSpells
 # import arrow
 # from cassiopeia.core import Summoner, MatchHistory, Match
-# from collections import Counter
-# import json
-# import time
-# import csv
+import sqlite3
+import pandas as pd
+import numpy as np
+from collections import Counter
 
 conn = sqlite3.connect('lol.db')
-
 def get_data(conn, table_name):
     res = []
     query = 'SELECT * FROM ' + table_name
@@ -23,24 +20,6 @@ def get_data(conn, table_name):
     data = c.execute(query)
     for d in data:
         res.append(d)
-    return res
-
-def chosen_rate(champion_id, participants):
-    data = {}
-    res = {}
-    for p in participants:
-        if p[3] == champion_id:
-            pos = get_position(p)
-            if pos is None:
-                continue
-            else:
-                if pos in data:
-                    data[pos]+=1
-                else:
-                    data[pos]=1
-    for each in data:
-        rate = data[each]/len(participants)
-        res[each] = rate
     return res
 
 def get_position(p):
@@ -53,25 +32,6 @@ def get_position(p):
         return p[7]
 
 
-def win_rate(champion_id, participants):
-    data = {}
-    res = {}
-    for p in participants:
-        if p[3] == champion_id:
-            pos = get_position(p)
-            if pos is None:
-                continue
-            else:
-                if pos in data:
-                    data[pos].append(p[5])
-                else:
-                    data[pos] = []
-                    data[pos].append(p[5])
-    for each in data:
-        rate = sum(data[each])/len(data[each])
-        res[each] = rate
-    return res
-
 def ban_rate(champion_id, team_ban):
     champions = []
     for ban in team_ban:
@@ -79,63 +39,14 @@ def ban_rate(champion_id, team_ban):
     rate = champions.count(champion_id)/len(team_ban)
     return rate
 
-def average_kda(champion_id, participants):
+def champion_data(champion_id, participants):
+    chosen_rate = {}
+    win_rate = {}
     kill = {}
     death = {}
     assist = {}
-    res = {}
-    for p in participants:
-        if p[3] == champion_id:
-            pos = get_position(p)
-            if pos is None:
-                continue
-            else:
-                if pos in kill:
-                    kill[pos].append(p[12])
-                    death[pos].append(p[13])
-                    assist[pos].append(p[14])
-                else:
-                    kill[pos] = []
-                    death[pos] = []
-                    assist[pos] = []
-                    kill[pos].append(p[12])
-                    death[pos].append(p[13])
-                    assist[pos].append(p[14])
-    for k in kill:
-        res[k] = sum(kill[k])/len(kill[k])
-    for d in death:
-        res[d] = sum(death[d])/len(death[d])
-    for a in assist:
-        res[a] = sum(assist[a])/len(assist[a])
-    
-    return res
-
-def damage(champion_id, participants):
     damage_to = {}
     damage_taken = {}
-    res1 = {}
-    res2 = {}
-    for p in participants:
-        if p[3] == champion_id:
-            pos = get_position(p)
-            if pos is None:
-                continue
-            else:
-                if pos in damage_to:
-                    damage_to[pos].append(p[-6])
-                    damage_taken[pos].append(p[-5])
-                else:
-                    damage_to[pos] = []
-                    damage_taken[pos] = []
-                    damage_to[pos].append(p[-6])
-                    damage_taken[pos].append(p[-5])
-    for d in damage_to:
-        res1[d] = sum(damage_to[d])/len(damage_to[d])
-    for d in damage_taken:
-        res2[d] = sum(damage_taken[d])/len(damage_taken[d])
-    return res1, res2
-    
-def money(champion_id, participants):
     gold = {}
     res = {}
     for p in participants:
@@ -144,40 +55,65 @@ def money(champion_id, participants):
             if pos is None:
                 continue
             else:
-                if pos in gold:
+                if pos in chosen_rate:
+                    chosen_rate[pos]+=1
+                    win_rate[pos].append(p[5])
+                    kill[pos].append(p[12])
+                    death[pos].append(p[13])
+                    assist[pos].append(p[14])
+                    damage_to[pos].append(p[-6])
+                    damage_taken[pos].append(p[-5])
                     gold[pos].append(p[26])
+                    
                 else:
+                    chosen_rate[pos] = 1
+                    win_rate[pos] = []
+                    kill[pos] = []
+                    death[pos] = []
+                    assist[pos] = []
+                    damage_to[pos] = []
+                    damage_taken[pos] = []
                     gold[pos] = []
+                    chosen_rate[pos]+=1
+                    win_rate[pos].append(p[5])
+                    kill[pos].append(p[12])
+                    death[pos].append(p[13])
+                    assist[pos].append(p[14])
+                    damage_to[pos].append(p[-6])
+                    damage_taken[pos].append(p[-5])
                     gold[pos].append(p[26])
+                    
+    for r in chosen_rate:
+        rate = chosen_rate[r]/len(participants)
+        res[r] = []
+        res[r].append({'chosen_rate': rate})
+    for r in win_rate:
+        rate = sum(win_rate[r])/len(win_rate[r])
+        res[r].append({'win_rate': rate})
+    for k in kill:
+        res[k].append({'kills': sum(kill[k])/len(kill[k])})
+    for d in death:
+        res[d].append({'deaths': sum(death[d])/len(death[d])})
+    for a in assist:
+        res[a].append({'assists': sum(assist[a])/len(assist[a])})
+    for d in damage_to:
+        res[d].append({'total_damage_to': sum(damage_to[d])/len(damage_to[d])})
+    for d in damage_taken:
+        res[d].append({'total_damage_taken': sum(damage_taken[d])/len(damage_taken[d])})
     for g in gold:
-        res[g] = sum(gold[g])/len(gold[g])
+        res[g].append({'gold_earned': sum(gold[g])/len(gold[g])})
+    
     return res
+
 
 
 participants = get_data(conn, 'Participants')
 champion = get_data(conn, 'Champion')
 ban = get_data(conn, 'team_ban')
 
-chosen_rates = []
-ban_rates = []
-win_rates = []
-average_kdas = []
-average_damage_to = []
-average_damage_taken = []
-moneys = []
-
+all_data = []
 for c in champion:
-    chosen_rates.append(chosen_rate(c[0], participants))
-    ban_rates.append(ban_rate(c[0], ban))
-    win_rates.append(win_rate(c[0], participants))
-    average_kdas.append(average_kda(c[0], participants))
-    average_damage_to.append(damage(c[0], participants)[0])
-    average_damage_taken.append(damage(c[0], participants)[1])
-    moneys.append(money(c[0], participants))
+    all_data.append(champion_data(c[0], participants))
+print(all_data)
 
-# print(chosen_rates)
-# print(win_rates)
-# print(max(ban_rates))
-# print(average_kdas)
-# print(moneys)
 
